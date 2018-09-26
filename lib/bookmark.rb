dcb31364fe1da83560f303403eefe942f6460094
AlexDresco <alexandra.d1510@gmail.com>
2018-09-26 14:53:07 +0100

    Typo

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -4,8 +4,8 @@ class Bookmark
   def self.all
     if ENV["RACK_ENV"] == 'test'
     connection = PG.connect(dbname: 'bookmark_manager_test')
-    else 
-    connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+    connection = PG.connect(dbname: 'bookmark_manager')
     end
 
     table = connection.exec('SELECT * FROM bookmarks')
