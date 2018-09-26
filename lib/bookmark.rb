2a2fe1c3700513450f7a5e290f63e3673eeb9bea
fbl11 <freyab_london@yahoo.co.uk>
2018-09-26 15:16:00 +0100

    adds user story and test for new feature: add bookmarks

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -3,9 +3,9 @@ require 'pg'
 class Bookmark
   def self.all
     if ENV["RACK_ENV"] == 'test'
-    connection = PG.connect(dbname: 'bookmark_manager_test')
+      connection = PG.connect(dbname: 'bookmark_manager_test')
     else
-    connection = PG.connect(dbname: 'bookmark_manager')
+      connection = PG.connect(dbname: 'bookmark_manager')
     end
 
     table = connection.exec('SELECT * FROM bookmarks')
--- spec/feature/viewing_bookmarks_spec.rb
+++ spec/feature/viewing_bookmarks_spec.rb
@@ -15,4 +15,15 @@ feature 'Viewing bookmarks' do
     expect(bookmarks).to have_content 'http://www.destroyallsoftware.com'
     expect(bookmarks).to have_content 'http://www.google.com'
   end
+
+  feature 'Adding bookmarks' do
+    scenario 'A user adds a bookmark' do
+      visit('/bookmarks')
+      expect(page.status_code).to eq(200)
+      click_button('Add link')
+      expect(page).have_content 'Add url here:'
+      fill_in('URL', with: 'http://www.amazon.com')
+      click_button('Submit')
+    end
+  end
 end
